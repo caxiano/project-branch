00b176b0909fef34212e0be26e6875c413bbc38e
Cassiano Shigueyuki Nishikawa <cassiano.nishikawa@gmail.com>
2025-03-14 21:42:45 -0300

    Updates

--- class/aula117.py
+++ class/aula117.py
@@ -1,13 +1,13 @@
 import json
 
 pessoa = {
-    'nome': 'Luiz Otávio 2',
-    'sobrenome': 'Miranda',
+    'nome': 'Cassiano Shigueyuki',
+    'sobrenome': 'Nishikawa',
     'enderecos': [
         {'rua': 'R1', 'numero': 32},
         {'rua': 'R2', 'numero': 55},
     ],
-    'altura': 1.8,
+    'altura': 1.7,
     'numeros_preferidos': (2, 4, 6, 8, 10),
     'dev': True,
     'nada': None,
