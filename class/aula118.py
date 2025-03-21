436376cadfa889b903401abc0adc191bb86dba1e
Cassiano Shigueyuki Nishikawa <cassiano.nishikawa@gmail.com>
2025-03-20 22:01:37 -0300

    Commit aulas

--- class/aula118.py
+++ class/aula118.py
@@ -1,3 +1,14 @@
+#Problema dos paraâmetros mutáveis em funções Python
+#O problema de passar uma lista como parâmetro para uma função
+#é que se você alterar o valor da lista dentro da função, a lista
+#será alterada fora da função também. Isso acontece porque a lista
+#é um objeto mutável em Python. Se você não quer que isso aconteça,
+#você pode passar uma cópia da lista para a função, ou passar uma
+#lista vazia e adicionar os elementos a ela dentro da função.
+#Exemplo de passagem de lista como parâmetro para uma função
+#que adiciona elementos a lista
+#def adiciona_clientes(nome, lista=None):
+
 def adiciona_clientes(nome, lista=None):
     if lista is None:
         lista = []
